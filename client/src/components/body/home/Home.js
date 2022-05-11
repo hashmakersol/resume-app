4d15f388113733e460875d150a14baa652b51c72
sherazmaker <sheraz.q@hashmakersol.net>
2022-05-11 13:45:45 +0500

    MERN stack Full Auth

---
+++ client/src/components/body/home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className='home_page'>
+      <h2>Online resume builder application</h2>
+    </div>
+  );
+}
+
+export default Home;
